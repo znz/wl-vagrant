f2f0457564c04b1279bf4c9a47061ae25e2026fd
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-08-16 20:26:08 +0900

    Add cron-apt

--- provision-packages.sh
+++ provision-packages.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 export DEBIAN_FRONTEND=noninteractive
 apt-get purge -y nano
 apt-get install -y vim
+apt-get install -y cron-apt
 apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
 apt-get install -y byobu ccache curl lv zsh
 apt-get install -y golang
