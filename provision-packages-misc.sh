862528b999d64ccc9109cf3a3a2b53b948d4d290
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-08-16 23:36:36 +0900

    Install source-highlight

--- provision-packages-misc.sh
+++ provision-packages-misc.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 export DEBIAN_FRONTEND=noninteractive
+apt-get install -y lv
 apt-get install -y silversearcher-ag
+apt-get install -y source-highlight
+apt-get install -y zsh
--- provision-packages.sh
+++ provision-packages.sh
@@ -5,6 +5,8 @@ apt-get purge -y nano
 apt-get install -y vim
 apt-get install -y cron-apt
 apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
-apt-get install -y byobu ccache curl lv zsh
+apt-get install -y byobu
+apt-get install -y ccache
+apt-get install -y curl
 apt-get install -y golang
 apt-get install -y subversion
