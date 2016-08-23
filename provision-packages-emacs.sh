4c8dbd3946e879655db96f99ba9b14e3a4738e3a
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-08-23 23:50:57 +0900

    Install gpgsm, gnupg-agent, and editorconfig

--- provision-packages-emacs.sh
+++ provision-packages-emacs.sh
@@ -4,3 +4,5 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get install -y emacs24 emacs24-el fonts-ipafont fonts-takao fonts-vlgothic
 apt-get install -y apel flim semi wl-beta gnutls-bin x-face-el w3m-el mu-cite mhc
 apt-get install -y hunspell hunspell-en-us
+apt-get install -y gpgsm gnupg-agent
+apt-get install -y editorconfig
