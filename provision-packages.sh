1a2fc36fd0a57a54e9e948e1412ef20ea64ae9e4
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-08-23 23:51:16 +0900

    Install aptitude

--- provision-packages.sh
+++ provision-packages.sh
@@ -4,6 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get purge -y nano
 apt-get install -y vim
 apt-get install -y cron-apt
+apt-get install -y aptitude
 apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
 apt-get install -y byobu
 apt-get install -y ccache
