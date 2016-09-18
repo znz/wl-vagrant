b5517f4fd0b027d932dc6495804cd56c752014d3
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-09-18 19:02:58 +0900

    Add rdtool-elisp

--- provision-packages-emacs.sh
+++ provision-packages-emacs.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 export DEBIAN_FRONTEND=noninteractive
 apt-get install -y emacs24 emacs24-el fonts-ipafont fonts-takao fonts-vlgothic
 apt-get install -y apel flim semi wl-beta gnutls-bin x-face-el w3m-el mu-cite mhc
+apt-get install -y rdtool-elisp
 apt-get install -y hunspell hunspell-en-us
 apt-get install -y gpgsm gnupg-agent
 apt-get install -y editorconfig
