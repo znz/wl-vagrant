a72369c0fb67acb544c5d221545d2f60fe15acd3
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-10-11 21:20:31 +0900

    Add `-x -u` to `go get`

--- provision-go.sh
+++ provision-go.sh
@@ -2,8 +2,8 @@
 set -euxo pipefail
 export GOPATH="$HOME/g"
 if [[ ! -x "$HOME/g/bin/ghq" ]]; then
-  go get github.com/motemen/ghq
+  go get -x -u github.com/motemen/ghq
 fi
 if [[ ! -x "$HOME/g/bin/peco" ]]; then
-  go get github.com/peco/peco/cmd/peco
+  go get -x -u github.com/peco/peco/cmd/peco
 fi
