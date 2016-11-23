f17087884c74f4256e22e600e6b44e3160081558
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-11-23 17:01:08 +0900

    Update packages

--- provision-packages-misc.sh
+++ provision-packages-misc.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -euxo pipefail
 export DEBIAN_FRONTEND=noninteractive
+apt-get install -y aptitude
+apt-get install -y clang
+apt-get install -y golang
 apt-get install -y lv
+apt-get install -y shellcheck
 apt-get install -y silversearcher-ag
 apt-get install -y source-highlight
-apt-get install -y zsh
+apt-get install -y subversion
 apt-get install -y vim-editorconfig
+apt-get install -y zsh
--- provision-packages.sh
+++ provision-packages.sh
@@ -3,13 +3,12 @@ set -euxo pipefail
 export DEBIAN_FRONTEND=noninteractive
 apt-get purge -y nano
 apt-get install -y vim
-apt-get install -y aptitude
 apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
 apt-get install -y byobu
 apt-get install -y ccache
+apt-get install -y clang
 apt-get install -y curl
-apt-get install -y golang
-apt-get install -y subversion
+apt-get install -y ruby
 echo unattended-upgrades unattended-upgrades/enable_auto_updates boolean true | debconf-set-selections
 apt-get install -y unattended-upgrades
 sed -i -e 's,//\(Unattended-Upgrade::Remove-Unused-Dependencies "\)false\(";\),\1true\2,' /etc/apt/apt.conf.d/50unattended-upgrades
